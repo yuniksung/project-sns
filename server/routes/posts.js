87f1e82732e4a7d35dadac3a3e3578a10fc17b58
Joshua Sung <yuniksung@gmail.com>
2021-05-16 07:53:25 -0500

    likePost function added to the post, restructured all codes and delete, create, update, like functions are all working right away

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 // routes
-import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
@@ -10,5 +10,7 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
