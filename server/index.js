930b4da68d01957cd4c050f29782b814e8321e0b
Joshua Sung <yuniksung@gmail.com>
2021-05-15 20:15:34 -0500

    routes, controllers, and models created for 'new posts'

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended:true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended:true }));
 app.use(cors());
 
 
-const CONNECTION_URL = 
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
