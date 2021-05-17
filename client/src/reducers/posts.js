e7bf41d6481a06e25aad53b87bd974c5b46e1d75
Joshua Sung <yuniksung@gmail.com>
2021-05-17 15:42:08 -0500

    having an issue on 'moment' has fixed

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
+// eslint-disable-next-line
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -16,6 +16,7 @@ const postSchema = mongoose.Schema({
     },
 })
 
+
 var PostMessage = mongoose.model('PostMessage', postSchema);
 
 export default PostMessage;
